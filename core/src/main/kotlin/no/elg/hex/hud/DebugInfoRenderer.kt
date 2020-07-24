3f3bd1135b4d146ff2debd6d93ac6fc799eb0968
Karl Henrik Elg <klink100@gmail.com>
2020-07-24 20:37:34 +0200

    Display cursor hexagon coordinates

--- core/src/main/kotlin/no/elg/hex/hud/DebugInfoRenderer.kt
+++ core/src/main/kotlin/no/elg/hex/hud/DebugInfoRenderer.kt
@@ -21,13 +21,16 @@ object DebugInfoRenderer : FrameUpdatable {
 
     val screenPos = "Screen pos (%4d,%4d)".format(Gdx.input.x, Gdx.input.y)
     val realPos = "Real pos (% 5.0f,% 5.0f)".format(mouseX, mouseY)
+    val cursorHex = cursorHex
     drawAll(
       variableText("FPS: ", Gdx.graphics.framesPerSecond, 30, Int.MAX_VALUE, format = { "%4d".format(it) },
         next = variableText(" zoom: ", Hex.camera.zoom, MIN_ZOOM, MAX_ZOOM, format = { "%.2f".format(it) })),
       ScreenText("Island is ${Hex.island.grid.gridData.gridWidth} x ${Hex.island.grid.gridData.gridHeight} ${Hex.island.grid.gridData.gridLayout}"),
       ScreenText(screenPos),
       ScreenText(realPos),
-      ScreenText("Pointing at hex ", next = nullCheckedText(cursorHex?.getData(), color = Color.YELLOW))
+      ScreenText("Pointing at hex ", next = nullCheckedText(cursorHex?.getData(), color = Color.YELLOW, format = {
+        "( %2d, % 2d, % 2d) $it".format(cursorHex?.gridX, cursorHex?.gridY, cursorHex?.gridZ)
+      }))
     )
   }
 }
