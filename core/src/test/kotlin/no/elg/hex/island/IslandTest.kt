61d39e9b83b8da9d439291f75b0a63e340872edd
Karl Henrik Elg <klink100@gmail.com>
2020-07-25 12:38:44 +0200

    Minor changes

---
+++ core/src/test/kotlin/no/elg/hex/island/IslandTest.kt
@@ -0,0 +1,21 @@
+package no.elg.hex.island
+
+import com.badlogic.gdx.files.FileHandle
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.io.File
+
+/**
+ * @author Elg
+ */
+internal class IslandTest {
+
+
+  private fun getIsFile(filename: String): FileHandle = FileHandle(File("./src/test/resources/$filename"))
+
+  @Test
+  fun `calculateBestCapitalPlacement() All same color should return center`() {
+    val loaded = Island.loadIsland(getIsFile("island-all-sun-hexagonal-radius-3.is"))
+    assertTrue(loaded)
+  }
+}
