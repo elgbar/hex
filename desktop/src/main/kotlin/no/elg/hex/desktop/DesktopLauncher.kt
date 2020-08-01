20a538afaf0101c44c66dd219e06842de0958df6
Karl Henrik Elg <klink100@gmail.com>
2020-08-01 12:08:07 +0200

    Add icon

--- desktop/src/main/kotlin/no/elg/hex/desktop/DesktopLauncher.kt
+++ desktop/src/main/kotlin/no/elg/hex/desktop/DesktopLauncher.kt
@@ -1,5 +1,6 @@
 package no.elg.hex.desktop
 
+import com.badlogic.gdx.Files.FileType.Internal
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import com.xenomachina.argparser.ArgParser
@@ -22,6 +23,8 @@ fun main(args: Array<String>) {
   config.vSyncEnabled = false //Why not? it's not like this is a competitive FPS
   config.samples = 16 //max out the samples as this isn't a very heavy game.
   config.useHDPI = true
+  config.addIcon("icons/icon32.png", Internal)
+  config.addIcon("icons/icon128.png", Internal)
 
   Hex.args = ArgParser(args).parseInto(::ApplicationArgumentsParser)
 
