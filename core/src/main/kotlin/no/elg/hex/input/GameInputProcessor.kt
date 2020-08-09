abdefb448414ecb700a8e53bdf065a1cc647a370
Karl Henrik Elg <klink100@gmail.com>
2020-08-09 21:56:47 +0200

    Refactor out placing and picking up pieces

--- core/src/main/kotlin/no/elg/hex/input/GameInputProcessor.kt
+++ core/src/main/kotlin/no/elg/hex/input/GameInputProcessor.kt
@@ -21,15 +21,92 @@ import no.elg.hex.hexagon.Spearman
 import no.elg.hex.hexagon.strengthToType
 import no.elg.hex.island.Hand
 import no.elg.hex.island.Island
+import no.elg.hex.island.Territory
 import no.elg.hex.screens.IslandScreen
 import no.elg.hex.util.canAttack
 import no.elg.hex.util.getData
 import no.elg.hex.util.getNeighbors
+import org.hexworks.mixite.core.api.Hexagon
 
 /** @author Elg */
 class GameInputProcessor(private val islandScreen: IslandScreen) : InputAdapter() {
 
   var infiniteMoney = Hex.args.cheating
+
+  fun pickUp(island: Island, cursorHexData: HexagonData, territory: Territory) {
+    val cursorPiece = cursorHexData.piece
+    if (cursorPiece.movable &&
+        cursorPiece is LivingPiece &&
+        !cursorPiece.moved &&
+        cursorHexData.team == island.currentTeam) {
+      // We currently don't hold anything in our hand, so pick it up!
+      island.inHand = Hand(territory, cursorPiece)
+      cursorHexData.setPiece(Empty::class)
+    }
+    Gdx.app.debug("PLACE", "Hand was null, now it is ${island.inHand}")
+  }
+
+  fun placeDown(
+      island: Island,
+      territory: Territory,
+      placeOn: Hexagon<HexagonData>,
+      newPiece: Piece,
+      oldTerritory: Territory?
+  ) {
+    val hexData = island.getData(placeOn)
+    if (hexData.team != island.currentTeam && !territory.enemyBorderHexes.contains(placeOn)) {
+      Gdx.app.debug("PLACE", "Tried to place piece on enemy hex outside border hexes")
+      return
+    }
+
+    val oldPiece = hexData.piece
+
+    val (newPieceType, moved) = if (oldPiece is LivingPiece &&
+        newPiece is LivingPiece &&
+        hexData.team == territory.team) {
+      // merge cursor piece with held piece
+      val newStr = newPiece.strength + oldPiece.strength
+      if (newStr > BARON_STRENGTH) return // cannot merge
+      // The piece can only move when both the piece in hand and the hex pointed at has not moved
+      strengthToType(newStr) to (newPiece.moved || oldPiece.moved)
+    } else {
+      newPiece::class to (hexData.team != island.currentTeam || oldPiece !is Empty)
+    }
+
+    if (newPieceType.isSubclassOf(LivingPiece::class)) {
+      if (hexData.team == territory.team && (oldPiece is Capital || oldPiece is Castle)) {
+        Gdx.app
+            .debug(
+                "PLACE",
+                "Cannot place a living entity of the same team onto a capital or castle piece")
+        return
+      } else if (hexData.team != territory.team && !island.canAttack(placeOn, newPiece)) {
+        Gdx.app
+            .debug(
+                "PLACE",
+                "Cannot attack ${oldPiece::class.simpleName} with a ${newPiece::class.simpleName}")
+        return
+      }
+    } else if (Castle::class != newPieceType) {
+      throw IllegalStateException(
+          "Holding illegal piece '$newPieceType', can only hold living pieces and castle!")
+    }
+
+    if (hexData.setPiece(newPieceType)) {
+      hexData.team = territory.team
+
+      island.inHand?.holding = oldTerritory != territory
+      val updatedPiece = hexData.piece
+      if (updatedPiece is LivingPiece) {
+        updatedPiece.moved = moved
+      }
+      for (neighbor in island.getNeighbors(placeOn)) {
+        island.select(neighbor)
+      }
+      // reselect territory to update it's values
+      island.select(placeOn)
+    }
+  }
 
   override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
     if (islandScreen.island.currentTeam != Island.STARTING_TEAM) return false
@@ -49,77 +126,12 @@ class GameInputProcessor(private val islandScreen: IslandScreen) : InputAdapter(
             Gdx.app.debug("PLACE", "Territory is still null after selecting it")
             return true
           }
-          val cursorPiece = cursorHexData.piece
 
           val hand = island.inHand
           if (hand == null) {
-            if (cursorPiece.movable &&
-                cursorPiece is LivingPiece &&
-                !cursorPiece.moved &&
-                cursorHexData.team == island.currentTeam) {
-              // We currently don't hold anything in our hand, so pick it up!
-              island.inHand = Hand(territory, cursorPiece)
-              cursorHexData.setPiece(Empty::class)
-            }
-            Gdx.app.debug("PLACE", "Hand was null, now it is ${island.inHand}")
-            return true
-          }
-
-          if (cursorHexData.team != island.currentTeam &&
-              !territory.enemyBorderHexes.contains(cursorHex)) {
-            Gdx.app.debug("PLACE", "Tried to place piece on enemy hex outside border hexes")
-            return true
-          }
-
-          val handPiece = hand.piece
-
-          val (newPieceType, moved) = if (cursorPiece is LivingPiece &&
-              handPiece is LivingPiece &&
-              cursorHexData.team == hand.territory.team) {
-            // merge cursor piece with held piece
-            val newStr = handPiece.strength + cursorPiece.strength
-            if (newStr > BARON_STRENGTH) return true // cannot merge
-            // The piece can only move when both the piece in hand and the hex pointed at has not
-            // moved
-            strengthToType(newStr) to (handPiece.moved || cursorPiece.moved)
+            pickUp(island, cursorHexData, territory)
           } else {
-            handPiece::class to (cursorHexData.team != island.currentTeam || cursorPiece !is Empty)
-          }
-
-          if (newPieceType.isSubclassOf(LivingPiece::class)) {
-            if (cursorHexData.team == hand.territory.team &&
-                (cursorPiece is Capital || cursorPiece is Castle)) {
-              Gdx.app
-                  .debug(
-                      "PLACE",
-                      "Cannot place a living entity of the same team onto a capital or castle piece")
-              return true
-            } else if (cursorHexData.team != hand.territory.team &&
-                !island.canAttack(cursorHex, handPiece)) {
-              Gdx.app
-                  .debug(
-                      "PLACE",
-                      "Cannot attack ${cursorPiece::class.simpleName} with a ${handPiece::class.simpleName}")
-              return true
-            }
-          } else if (Castle::class != newPieceType) {
-            throw IllegalStateException(
-                "Holding illegal piece '$newPieceType', can only hold living pieces and castle!")
-          }
-
-          if (cursorHexData.setPiece(newPieceType)) {
-            cursorHexData.team = hand.territory.team
-
-            island.inHand?.holding = oldTerritory != territory
-            val newPiece = cursorHexData.piece
-            if (newPiece is LivingPiece) {
-              newPiece.moved = moved
-            }
-            for (neighbor in island.getNeighbors(cursorHex)) {
-              island.select(neighbor)
-            }
-            // reselect territory to update it's values
-            island.select(cursorHex)
+            placeDown(island, territory, cursorHex, hand.piece, oldTerritory)
           }
         }
         else -> return false
