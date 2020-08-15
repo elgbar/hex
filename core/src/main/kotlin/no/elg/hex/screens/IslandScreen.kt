1c51f55a78c203cbf54b30b88b2f9860348506ab
Karl Henrik Elg <klink100@gmail.com>
2020-08-16 01:32:19 +0200

    Properly zoom and position island camera on resize

--- core/src/main/kotlin/no/elg/hex/screens/IslandScreen.kt
+++ core/src/main/kotlin/no/elg/hex/screens/IslandScreen.kt
@@ -1,23 +1,49 @@
 package no.elg.hex.screens
 
 import com.badlogic.gdx.InputProcessor
-import java.lang.Float.max
+import kotlin.math.max
 import no.elg.hex.Hex
 import no.elg.hex.api.FrameUpdatable
 import no.elg.hex.hud.DebugInfoRenderer
 import no.elg.hex.hud.GameInfoRenderer
 import no.elg.hex.hud.MapEditorRenderer
 import no.elg.hex.input.BasicIslandInputProcessor
+import no.elg.hex.input.BasicIslandInputProcessor.Companion.MAX_ZOOM
+import no.elg.hex.input.BasicIslandInputProcessor.Companion.MIN_ZOOM
 import no.elg.hex.input.GameInputProcessor
 import no.elg.hex.input.MapEditorInputProcessor
 import no.elg.hex.island.Island
 import no.elg.hex.renderer.OutlineRenderer
 import no.elg.hex.renderer.SpriteRenderer
 import no.elg.hex.renderer.VerticesRenderer
+import no.elg.hex.util.component6
+import no.elg.hex.util.component7
+import no.elg.hex.util.component8
+import no.elg.hex.util.getData
 
 /** @author Elg */
 class IslandScreen(val id: Int, val island: Island, private val renderHud: Boolean = true) :
     AbstractScreen() {
+
+  private fun calcVisibleGridSize(): DoubleArray {
+    val data = island.grid.gridData
+    val visible = island.hexagons.filterNot { island.getData(it).invisible }
+
+    val minX = visible.minBy { it.center.coordinateX }!!.center.coordinateX
+    val maxX = visible.maxBy { it.center.coordinateX }!!.center.coordinateX
+
+    val minY = visible.minBy { it.center.coordinateY }!!.center.coordinateY
+    val maxY = visible.maxBy { it.center.coordinateY }!!.center.coordinateY
+
+    val maxInvX = island.hexagons.maxBy { it.center.coordinateX }!!.center.coordinateX
+    val minInvX = island.hexagons.minBy { it.center.coordinateX }!!.center.coordinateX
+
+    val maxInvY = island.hexagons.maxBy { it.center.coordinateY }!!.center.coordinateY
+    val minInvY = island.hexagons.minBy { it.center.coordinateY }!!.center.coordinateY
+    return doubleArrayOf(maxX, minX, maxY, minY, maxInvX, minInvX, maxInvY, minInvY)
+  }
+
+  private val visibleGridSize by lazy { calcVisibleGridSize() }
 
   val inputProcessor: InputProcessor by lazy {
     if (Hex.args.mapEditor) {
@@ -63,16 +89,29 @@ class IslandScreen(val id: Int, val island: Island, private val renderHud: Boole
     super.resize(width, height)
     val data = island.grid.gridData
 
-    val totalGridWidth = (data.gridWidth * data.hexagonWidth + data.gridWidth).toFloat()
-    val totalGridHeight = (data.gridHeight * data.hexagonHeight + data.gridHeight).toFloat()
+    val (maxX, minX, maxY, minY, maxInvX, minInvX, maxInvY, minInvY) = if (Hex.args.mapEditor)
+        calcVisibleGridSize()
+    else visibleGridSize
 
-    val x = totalGridWidth / 2f
-    val y = totalGridHeight / 2f
+    // Sum the distance from the edge of the grid to the first visible hexagon
+    // |.###..| (`.` are invisible, `#` are visible hexagons)
+    // The offset would then be 3
+    val gridWidthOffset = minInvX - minX + maxInvX - maxX
+    val gridHeightOffset = minInvY - minY + maxInvY - maxY
 
-    camera.position.x = x
-    camera.position.y = y
-    camera.zoom =
-        max(totalGridWidth / camera.viewportWidth, totalGridHeight / camera.viewportHeight)
+    // TBH I do not know what drives the extra width size
+    val islandCenterX = ((data.gridWidth + 0.5 - 0.15) * data.hexagonWidth - gridWidthOffset) / 2
+    val islandCenterY = ((data.gridHeight + 0.5) * data.hexagonHeight - gridHeightOffset) / 2
+
+    camera.position.x = islandCenterX.toFloat()
+    camera.position.y = islandCenterY.toFloat()
+
+    // Add some padding as the min/max x/y are calculated from the center of the hexagons
+    val padding = 2
+    val widthZoom = (maxX - minX + padding * data.hexagonWidth) / camera.viewportWidth
+    val heightZoom = (maxY - minY + padding * data.hexagonHeight) / camera.viewportHeight
+
+    camera.zoom = max(widthZoom, heightZoom).toFloat().coerceIn(MIN_ZOOM, MAX_ZOOM)
   }
 
   override fun show() {
---
+++ core/src/main/kotlin/no/elg/hex/util/ArrayExtra.kt
@@ -0,0 +1,7 @@
+package no.elg.hex.util
+
+operator fun DoubleArray.component6() = this[5]
+
+operator fun DoubleArray.component7() = this[6]
+
+operator fun DoubleArray.component8() = this[7]
