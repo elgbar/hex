c12e550448428ce1bb144c035139baf8806aba5f
Karl Henrik Elg <klink100@gmail.com>
2020-08-21 21:52:48 +0200

    Fix creation preview image being too stretched out

--- core/src/main/kotlin/no/elg/hex/screens/LevelCreationScreen.kt
+++ core/src/main/kotlin/no/elg/hex/screens/LevelCreationScreen.kt
@@ -1,6 +1,8 @@
 package no.elg.hex.screens
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input.Keys
+import com.badlogic.gdx.InputAdapter
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
@@ -10,17 +12,15 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 import com.badlogic.gdx.utils.Array as GdxArray
-import com.badlogic.gdx.utils.Scaling
+import com.badlogic.gdx.utils.Scaling.fit
 import com.badlogic.gdx.utils.viewport.ScalingViewport
 import com.kotcrab.vis.ui.util.form.FormInputValidator
-import com.kotcrab.vis.ui.widget.ButtonBar.ButtonType.APPLY
-import com.kotcrab.vis.ui.widget.ButtonBar.ButtonType.CANCEL
 import com.kotcrab.vis.ui.widget.spinner.ArraySpinnerModel
 import com.kotcrab.vis.ui.widget.spinner.IntSpinnerModel
 import ktx.actors.onChangeEvent
 import ktx.actors.onClick
 import ktx.scene2d.actors
-import ktx.scene2d.vis.buttonBar
+import ktx.scene2d.horizontalGroup
 import ktx.scene2d.vis.spinner
 import ktx.scene2d.vis.validator
 import ktx.scene2d.vis.visImage
@@ -28,6 +28,8 @@ import ktx.scene2d.vis.visTable
 import ktx.scene2d.vis.visTextButton
 import no.elg.hex.Hex
 import no.elg.hex.island.Island
+import no.elg.hex.island.Island.Companion.STARTING_TEAM
+import no.elg.hex.util.getData
 import no.elg.hex.util.play
 import org.hexworks.mixite.core.api.HexagonalGridLayout
 
@@ -36,16 +38,34 @@ object LevelCreationScreen : AbstractScreen() {
 
   val stage =
       Stage(
-          ScalingViewport(
-              Scaling.fillX, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat(), camera))
+          ScalingViewport(fit, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat(), camera))
 
   init {
+    resetActors()
+  }
 
+  val inputListener =
+      object : InputAdapter() {
+        override fun keyDown(keycode: Int): Boolean {
+          when (keycode) {
+            Keys.ESCAPE -> Hex.screen = LevelSelectScreen
+            else -> return false
+          }
+          return true
+        }
+      }
+
+  fun resetActors() {
+    stage.actors.clear()
     stage.actors {
-      visTable(true) {
+      visTable(defaultSpacing = true) {
+        defaults().pad(20f)
+        setFillParent(true)
+        if (Hex.debug) {
+          debug()
+        }
         validator {
           setFillParent(true)
-
           val layoutSpinner: ArraySpinnerModel<HexagonalGridLayout> =
               ArraySpinnerModel(GdxArray(HexagonalGridLayout.values()))
           val widthSpinner = IntSpinnerModel(10, 1, Int.MAX_VALUE)
@@ -53,13 +73,26 @@ object LevelCreationScreen : AbstractScreen() {
 
           var previewBuffer: FrameBuffer? = null
 
-          fun createIsland(): Island =
-              Island(widthSpinner.value + 2, heightSpinner.value + 2, layoutSpinner.current)
+          fun createIsland(isPreview: Boolean): Island {
+            return Island(widthSpinner.value + 2, heightSpinner.value + 2, layoutSpinner.current)
+                .also {
+                  if (isPreview) {
+                    for (hexagon in it.hexagons) {
+                      it.getData(hexagon).team = STARTING_TEAM
+                    }
+                  }
+                }
+          }
 
           val previewSize =
-              ((Gdx.graphics.width - 3 * (Gdx.graphics.width * 0.025f)) / 2).toInt() * 2
+              (((Gdx.graphics.width - 3 * (Gdx.graphics.width * 0.025f)) / 2).toInt() * 2)
+                  .coerceAtLeast(1024)
 
-          val previewImage = visImage(Texture(previewSize, previewSize, Pixmap.Format.RGBA8888)) {}
+          val previewImage =
+              visImage(Texture(previewSize, previewSize, Pixmap.Format.RGBA8888)) { cell ->
+                cell.colspan(3)
+                cell.expandX()
+              }
 
           fun renderPreview() {
             previewBuffer?.dispose()
@@ -69,14 +102,15 @@ object LevelCreationScreen : AbstractScreen() {
             layoutSpinner.setCurrent(layoutSpinner.current, false)
 
             if (layoutSpinner.current.gridLayoutStrategy.checkParameters(
-                widthSpinner.value, heightSpinner.value)) {
+                    widthSpinner.value, heightSpinner.value)) {
 
               previewBuffer =
-                  LevelSelectScreen.renderPreview(createIsland(), previewSize).also {
+                  LevelSelectScreen.renderPreview(createIsland(true), previewSize).also {
                     val region = TextureRegion(it.colorBufferTexture)
                     region.flip(false, true)
-
                     previewImage.drawable = TextureRegionDrawable(region)
+                    previewImage.inCell.fill(previewImage.height / previewImage.width, 0f)
+                    this@visTable.pack()
                   }
             }
           }
@@ -94,35 +128,39 @@ object LevelCreationScreen : AbstractScreen() {
               }
 
           spinner("Width", widthSpinner) {
+            inCell.uniform()
             textField.addValidator(validator)
             onChangeEvent { renderPreview() }
           }
           spinner("Height", heightSpinner) {
+            inCell.uniform()
             textField.addValidator(validator)
             onChangeEvent { renderPreview() }
           }
           spinner("Layout", layoutSpinner) {
+            inCell.fill(0.75f, 0f)
             textField.addValidator(validator)
             onChangeEvent { renderPreview() }
           }
 
           row()
-          val createButton =
-              visTextButton("Create island") {
-                onClick {
-                  Gdx.app.debug(
-                      "CREATOR",
-                      "Creating island ${LevelSelectScreen.islandAmount} with a dimension of ${widthSpinner.value} x ${heightSpinner.value} and layout ${layoutSpinner.current}")
-                  play(LevelSelectScreen.islandAmount, createIsland())
-                }
-              }
-          val cancelButton = visTextButton("Cancel") { onClick { Hex.screen = LevelSelectScreen } }
 
-          buttonBar {
-            setButton(APPLY, createButton)
-            setButton(CANCEL, cancelButton)
+          horizontalGroup {
+            it.colspan(3)
+            space(20f)
+
+            visTextButton("Create island") {
+              addDisableTarget(this)
+              onClick {
+                Gdx.app.debug(
+                    "CREATOR",
+                    "Creating island ${LevelSelectScreen.islandAmount} with a dimension of ${widthSpinner.value} x ${heightSpinner.value} and layout ${layoutSpinner.current}")
+                play(LevelSelectScreen.islandAmount, createIsland(false))
+              }
+            }
+
+            visTextButton("Cancel") { onClick { Hex.screen = LevelSelectScreen } }
           }
-          addDisableTarget(createButton)
         }
         pack()
       }
@@ -130,29 +168,35 @@ object LevelCreationScreen : AbstractScreen() {
   }
 
   override fun render(delta: Float) {
-    lineRenderer.begin(ShapeType.Line)
-    lineRenderer.color = Color.LIGHT_GRAY
-    lineRenderer.line(
-        Gdx.graphics.width / 2f, 0f, Gdx.graphics.width / 2f, Gdx.graphics.height.toFloat())
-    lineRenderer.line(
-        0f, Gdx.graphics.height / 2f, Gdx.graphics.width.toFloat(), Gdx.graphics.height / 2f)
-    lineRenderer.end()
-
     stage.act(delta)
     stage.draw()
+
+    if (Hex.debug) {
+      lineRenderer.begin(ShapeType.Line)
+      lineRenderer.color = Color.LIGHT_GRAY
+      lineRenderer.line(
+          Gdx.graphics.width / 2f, 0f, Gdx.graphics.width / 2f, Gdx.graphics.height.toFloat())
+      lineRenderer.line(
+          0f, Gdx.graphics.height / 2f, Gdx.graphics.width.toFloat(), Gdx.graphics.height / 2f)
+      lineRenderer.end()
+    }
   }
 
   override fun show() {
+    Hex.inputMultiplexer.addProcessor(inputListener)
     Hex.inputMultiplexer.addProcessor(stage)
   }
 
   override fun hide() {
+    Hex.inputMultiplexer.removeProcessor(inputListener)
     Hex.inputMultiplexer.removeProcessor(stage)
   }
 
   override fun resize(width: Int, height: Int) {
     super.resize(width, height)
     camera.setToOrtho(false)
-    stage.viewport.update(width, height, true)
+    stage.viewport.setWorldSize(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+    stage.viewport.setScreenSize(Gdx.graphics.width, Gdx.graphics.height)
+    resetActors()
   }
 }
