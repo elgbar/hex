28742597d2212c5f7aed4388f87b6aac853ad58b
Elg <klink100@gmail.com>
2020-12-27 14:35:43 +0100

    Never limit fps by default
    To make sure the application is shown in the best light by default

--- core/src/main/kotlin/no/elg/hex/Settings.kt
+++ core/src/main/kotlin/no/elg/hex/Settings.kt
@@ -1,15 +1,13 @@
 package no.elg.hex
 
-import com.badlogic.gdx.Application.ApplicationType.Android
-import com.badlogic.gdx.Gdx
 import no.elg.hex.util.delegate.PreferenceDelegate
 
 object Settings {
 
   var confirmEndTurn by PreferenceDelegate(true)
   var confirmSurrender by PreferenceDelegate(true)
-  var showFps by PreferenceDelegate(false)
 
-  var limitFps by PreferenceDelegate(Gdx.app.type == Android)
+  var showFps by PreferenceDelegate(false)
+  var limitFps by PreferenceDelegate(false)
   var targetFps by PreferenceDelegate(30) { it <= 0 }
 }
