d6497a1bcaa3bdb5f54431e7c02e965bc8bb3c42
Karl Henrik Elg <klink100@gmail.com>
2021-04-21 20:32:52 +0200

    Fix wrong variabel being printed as the piece class name

--- core/src/main/kotlin/no/elg/hex/jackson/serialization/HexagonDataIO.kt
+++ core/src/main/kotlin/no/elg/hex/jackson/serialization/HexagonDataIO.kt
@@ -21,12 +21,11 @@ class HexagonDataDeserializer(defaultSerializer: BeanDeserializer) : BeanDeseria
     val hexagonData = super.deserialize(parser, context) as HexagonData
 
     with(hexagonData) {
-
       // special loading required to properly serialize the piece on the hexagon
       // Jacksons default ordering is not reliable enough to allow loading of piece then the data for that piece.
       // A less hacky solution would be to serialize the pieces themself, but this is hard to do (see the #setPiece(class) method)
       if (loadedTypeName != null) {
-        val pieceClass = PIECES_MAP[loadedTypeName] ?: error("Unknown piece with the name $serializationDataToLoad")
+        val pieceClass = PIECES_MAP[loadedTypeName] ?: error("Unknown piece with the name $loadedTypeName")
         val pieceUpdated = setPiece(pieceClass) {
           this.handleDeserializationData(serializationDataToLoad)
         }
