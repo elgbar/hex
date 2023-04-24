e85ed0fdccdd5c1d589411ff02031855fe1e24e2
Karl Henrik Elg <klink100@gmail.com>
2023-04-24 19:03:41 +0200

    Tune difficulty

--- core/src/main/kotlin/no/elg/hex/ai/Difficulty.kt
+++ core/src/main/kotlin/no/elg/hex/ai/Difficulty.kt
@@ -9,7 +9,7 @@ import java.lang.Integer.MAX_VALUE
 enum class Difficulty(val aiConstructor: (Team) -> AI?) {
 
   PLAYER({ null }),
-  EASY({ NotAsRandomAI(it, MAX_VALUE, 0.1f) }),
-  NORMAL({ NotAsRandomAI(it, 5, 0.01f) }),
+  EASY({ NotAsRandomAI(it, MAX_VALUE, 0.01f) }),
+  NORMAL({ NotAsRandomAI(it, 4, 0.001f) }),
   HARD({ NotAsRandomAI(it, 0, 0.0001f) })
 }
