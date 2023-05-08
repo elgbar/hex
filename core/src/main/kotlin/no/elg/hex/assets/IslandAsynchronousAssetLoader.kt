1758a4ad7fa21d724f15a1c5e896a70a92d7c3a2
Karl Henrik Elg <klink100@gmail.com>
2023-05-08 08:43:36 +0200

    Remove redundant loading logic

--- core/src/main/kotlin/no/elg/hex/assets/IslandAsynchronousAssetLoader.kt
+++ core/src/main/kotlin/no/elg/hex/assets/IslandAsynchronousAssetLoader.kt
@@ -7,12 +7,10 @@ import com.badlogic.gdx.assets.loaders.FileHandleResolver
 import com.badlogic.gdx.files.FileHandle
 import no.elg.hex.Hex
 import no.elg.hex.Settings
-import no.elg.hex.hud.MessagesRenderer
 import no.elg.hex.island.Island
 import no.elg.hex.util.regenerateCapitals
 import no.elg.hex.util.reportTiming
 import no.elg.hex.util.serialize
-import org.hexworks.mixite.core.api.HexagonalGridLayout.HEXAGONAL
 
 /** @author Elg */
 class IslandAsynchronousAssetLoader(resolver: FileHandleResolver) :
@@ -46,23 +44,18 @@ class IslandAsynchronousAssetLoader(resolver: FileHandleResolver) :
       file.readString()
     }
     island =
-      val island = reportTiming("deserialize island json") {
+      reportTiming("deserialize island json") {
         Island.deserialize(json)
+      }.also { island ->
+        if (Hex.args.`update-saved-islands`) {
+          Gdx.files.local(file.path()).writeString(
+            island.also {
+              it.regenerateCapitals()
+              it.currentTeam = Settings.startTeam
+            }.createDto().serialize(),
+            false
+          )
+        }
       }
-      if (Hex.args.`update-saved-islands`) {
-        Gdx.files.local(file.path()).writeString(
-          island.also {
-            it.regenerateCapitals()
-            it.currentTeam = Settings.startTeam
-          }.createDto().serialize(),
-          false
-        )
-      }
-      island
-    } catch (e: Exception) {
-      MessagesRenderer.publishError("Invalid island save data for island '${file.name()}'")
-      Gdx.app.log("ISLAND LOADING", e.message)
-      Island(25, 25, HEXAGONAL).also { it.regenerateCapitals() }
-    }
   }
 }
