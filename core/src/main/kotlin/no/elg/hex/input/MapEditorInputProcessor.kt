cec8f00cc7381088203215cef5f25b48b371d310
Karl Henrik Elg <klink100@gmail.com>
2023-05-11 10:13:41 +0200

    Fix having to touch drag to edit hexagons

--- core/src/main/kotlin/no/elg/hex/input/MapEditorInputProcessor.kt
+++ core/src/main/kotlin/no/elg/hex/input/MapEditorInputProcessor.kt
@@ -2,6 +2,7 @@ package no.elg.hex.input
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input.Buttons
+import no.elg.hex.hexagon.HexagonData
 import no.elg.hex.input.editor.EditMetadata
 import no.elg.hex.input.editor.Editor
 import no.elg.hex.screens.MapEditorScreen
@@ -9,6 +10,7 @@ import no.elg.hex.util.calculateHexagonsWithinRadius
 import no.elg.hex.util.getData
 import no.elg.hex.util.trace
 import no.elg.hex.util.withData
+import org.hexworks.mixite.core.api.Hexagon
 
 /** @author Elg */
 class MapEditorInputProcessor(private val screen: MapEditorScreen) : AbstractInput() {
@@ -22,20 +24,43 @@ class MapEditorInputProcessor(private val screen: MapEditorScreen) : AbstractInp
       return metadata == null || (metadata.selectedPiece != screen.selectedPiece || metadata.selectedTeam != screen.selectedTeam || screen.editor != metaEditor)
     }
 
+  private fun postEdit(metadata: EditMetadata) {
+    if (isEditorChanged) {
+      currentMetadata = null
+      return
+    }
+    val cursorHex = screen.basicIslandInputProcessor.cursorHex ?: return
+
+    val editor = screen.editor
+    metaEditor = editor
+    val hexagons = screen.island.calculateHexagonsWithinRadius(cursorHex, screen.brushRadius - 1)
+    hexagons.withData(metadata.island, excludeInvisible = false) { hex, data ->
+      Gdx.app.trace("Editor") { "Editing ${hex.cubeCoordinate.toAxialKey()}" }
+      editor.edit(hex, data, metadata)
+    }
+    editor.postEdit(metadata)
+  }
+
+  private fun setEditorMetadata(cursorHex: Hexagon<HexagonData>): EditMetadata {
+    val island = screen.island
+    val clickedHexagonData = island.getData(cursorHex).copy()
+    metaEditor = screen.editor
+    return EditMetadata(
+      clickedHexagon = cursorHex,
+      clickedHexagonData = clickedHexagonData,
+      island = island,
+      selectedPiece = screen.selectedPiece,
+      selectedTeam = screen.selectedTeam
+    ).also {
+      currentMetadata = it
+    }
+  }
+
   override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
     if (button == Buttons.LEFT) {
-      val cursorHex = screen.basicIslandInputProcessor.cursorHex ?: return false
-
-      val island = screen.island
-      val clickedHexagonData = island.getData(cursorHex).copy()
-      metaEditor = screen.editor
-      currentMetadata = EditMetadata(
-        clickedHexagon = cursorHex,
-        clickedHexagonData = clickedHexagonData,
-        island = island,
-        selectedPiece = screen.selectedPiece,
-        selectedTeam = screen.selectedTeam
-      )
+      val cursorHex: Hexagon<HexagonData> = screen.basicIslandInputProcessor.cursorHex ?: return false
+      val editorMetadata = setEditorMetadata(cursorHex)
+      postEdit(editorMetadata)
       return true
     }
     return false
@@ -43,26 +68,12 @@ class MapEditorInputProcessor(private val screen: MapEditorScreen) : AbstractInp
 
   override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
     currentMetadata = null
-    return false
+    return true
   }
 
   override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
-    val metadata = currentMetadata
-    if (Gdx.input.isButtonPressed(Buttons.LEFT) && metadata != null) {
-      if (isEditorChanged) {
-        currentMetadata = null
-        return false
-      }
-      val cursorHex = screen.basicIslandInputProcessor.cursorHex ?: return false
-
-      val editor = screen.editor
-      metaEditor = editor
-      val hexagons = screen.island.calculateHexagonsWithinRadius(cursorHex, screen.brushRadius - 1)
-      hexagons.withData(metadata.island, excludeInvisible = false) { hex, data ->
-        Gdx.app.trace("Editor") { "Editing ${hex.cubeCoordinate.toAxialKey()}" }
-        editor.edit(hex, data, metadata)
-      }
-      editor.postEdit(metadata)
+    if (Gdx.input.isButtonPressed(Buttons.LEFT)) {
+      postEdit(currentMetadata ?: return false)
       return true
     }
     return false
