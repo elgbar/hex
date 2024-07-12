40d0b349d3732f11c06743cfbdf5604dc6e60fba
Karl Henrik Elg <klink100@gmail.com>
2024-07-12 22:08:17 +0200

    Repack sprites

--- core/src/main/kotlin/no/elg/hex/Assets.kt
+++ core/src/main/kotlin/no/elg/hex/Assets.kt
@@ -188,7 +188,7 @@ class Assets : AssetManager() {
   }
 
   val hand by lazy { findSprite("hand") }
-  val background by lazy { findSprite("slay_game_background") }
+  val background by lazy { findSprite("background") }
   val pine by lazy { findSprite("pine") }
   val palm by lazy { findSprite("palm") }
   val capital by lazy { findSprite("village") }
