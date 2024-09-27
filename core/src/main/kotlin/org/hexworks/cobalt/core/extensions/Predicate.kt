7e908e880a577351bfdedd8c8bf8d9d3b2bb81f3
Karl Henrik Elg <klink100@gmail.com>
2024-09-27 21:53:53 +0200

    Update ARtB to 31 for island 61

--- core/src/main/kotlin/org/hexworks/cobalt/core/extensions/Predicate.kt
+++ core/src/main/kotlin/org/hexworks/cobalt/core/extensions/Predicate.kt
@@ -6,7 +6,7 @@ package org.hexworks.cobalt.core.extensions
 
 typealias Predicate<T> = Function1<T, Boolean>
 
-infix fun <T> Predicate<T>.and(other: Predicate<in T>): Predicate<T> = {
+infix fun <T> Predicate<T>.and(other: Predicate<T>): Predicate<T> = {
   this(it) && other(it)
 }
 
@@ -14,6 +14,6 @@ fun <T> Predicate<T>.negate(): Predicate<T> = {
   !this(it)
 }
 
-infix fun <T> Predicate<T>.or(other: Predicate<in T>): Predicate<T> = {
+infix fun <T> Predicate<T>.or(other: Predicate<T>): Predicate<T> = {
   this(it) || other(it)
 }
