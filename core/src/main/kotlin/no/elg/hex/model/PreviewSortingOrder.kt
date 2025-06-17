0818bf75ad396b376b6aea0f498cacd138b30351
Karl Henrik Elg <klink100@gmail.com>
2025-06-17 19:41:28 +0200

    Add not tried sorting order

--- core/src/main/kotlin/no/elg/hex/model/PreviewSortingOrder.kt
+++ core/src/main/kotlin/no/elg/hex/model/PreviewSortingOrder.kt
@@ -2,20 +2,40 @@ package no.elg.hex.model
 
 import no.elg.hex.Settings
 import no.elg.hex.island.Island
-import no.elg.hex.util.toTitleCase
+import no.elg.hex.preview.PreviewModifier
 import java.util.Comparator.comparingInt
 import java.util.function.ToIntFunction
 
 enum class PreviewSortingOrder(private val rawComparator: Comparator<FastIslandMetadata>) {
+  /**
+   * Sort islands by when they were created, i.e., ID
+   */
   BY_ID(comparingInt(FastIslandMetadata::id)),
+
+  /**
+   * Sort islands by lowest amount of rounds known to beat it
+   */
   BY_AUTHOR_ROUNDS(byRound(FastIslandMetadata::authorRoundsToBeat).thenComparing(BY_ID.rawComparator)),
+
+  /**
+   * Sort the islands by how many rounds the user used to beat it
+   */
   BY_USER_ROUNDS(byRound(FastIslandMetadata::userRoundsToBeat).thenComparing(BY_ID.rawComparator)),
-  NOT_WON(notWon().thenComparing(BY_AUTHOR_ROUNDS.rawComparator))
+
+  /**
+   * Sort islands by whether the user have tried to beat them
+   */
+  NOT_WON(byModifier().thenComparing(byWon()).thenComparing(BY_AUTHOR_ROUNDS.rawComparator)),
+
+  /**
+   * Sort islands by whether the user have tried to beat them
+   */
+  NOT_TRIED(byHaveTried().thenComparing(byWon()).thenComparing(BY_AUTHOR_ROUNDS.rawComparator))
   ;
 
   private val reversedComparator by lazy { rawComparator.reversed() }
 
-  override fun toString(): String = super.toString().replace('_', ' ').lowercase().toTitleCase()
+  override fun toString(): String = super.toString().replace('_', ' ') // .lowercase().toTitleCase()
 
   fun comparator(): Comparator<FastIslandMetadata> =
     if (Settings.reverseLevelSorting) {
@@ -37,13 +57,37 @@ private fun byRound(roundsToBeat: FastIslandMetadata.() -> Int): Comparator<Fast
     }
   )
 
-private fun notWon(): Comparator<FastIslandMetadata> =
+private fun byWon(): Comparator<FastIslandMetadata> =
   comparingInt(
     ToIntFunction<FastIslandMetadata> { metadata ->
       if (Island.NEVER_PLAYED == metadata.userRoundsToBeat) {
         0
       } else {
         Int.MAX_VALUE
+      }
+    }
+  )
+
+private fun byHaveTried(): Comparator<FastIslandMetadata> =
+  comparingInt(
+    ToIntFunction<FastIslandMetadata> { metadata ->
+      if (metadata.modifier == PreviewModifier.NOTHING) {
+        0
+      } else {
+        Int.MAX_VALUE
+      }
+    }
+  )
+
+private fun byModifier(): Comparator<FastIslandMetadata> =
+  comparingInt(
+    ToIntFunction<FastIslandMetadata> { metadata ->
+      when (metadata.modifier) {
+        PreviewModifier.SURRENDER -> 1
+        PreviewModifier.LOST -> 2
+        PreviewModifier.AI_DONE -> 3
+        PreviewModifier.NOTHING -> 4
+        PreviewModifier.WON -> 10
       }
     }
   )
